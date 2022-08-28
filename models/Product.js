d9587cecc3fa3eedc2aca1e43e031f2611f47ca1
Avni <avni0000patel@gmail.com>
2022-08-27 20:45:28 -0400

    Product price

--- models/Product.js
+++ models/Product.js
@@ -1,10 +1,11 @@
 // import important parts of sequelize library
+const { DECIMAL } = require('sequelize');
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
@@ -19,6 +20,13 @@ Product.init(
     product_name: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
     }
   },
   {
