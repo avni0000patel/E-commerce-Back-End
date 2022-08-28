2bf895ff13f44cbe8ca6bb0e0ce2f28155d9d660
Avni <avni0000patel@gmail.com>
2022-08-28 19:25:36 -0400

    remove } and attributes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,19 +1,18 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
-// The `/api/tags` endpoint
+// The `/api/tags` endpoint-
 
 router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product, attributes: ["id", "product_name", "price", "stock", "category_id"]}]
+      include: [{ model: Product }]
     });
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
-  }
   }
 });
 
@@ -22,7 +21,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product, attributes: ["id", "product_name", "price", "stock", "category_id"]}]
+      include: [{ model: Product }]
     });
     if (!tagData) {
       res.status(404).json({ message: "No tag with this id" });
