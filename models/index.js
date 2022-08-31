fcb109f453ccfc8be93b19476005c0eab5975d84
Avni <avni0000patel@gmail.com>
2022-08-30 22:23:46 -0400

    fix error foreign key constraint failed

--- models/index.js
+++ models/index.js
@@ -17,13 +17,13 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: "tag_id"
+  foreignKey: "product_id"
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey: "key_id"
+  foreignKey: "tag_id"
 })
 
 module.exports = {
